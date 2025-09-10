a518d6e49b7d32b9ec4bed9637cc6c8f718121ad
kashyappp__ <sheshendrakashyap06@gmail.com>
2025-09-10 21:41:15 +0530

    Update app main.py

--- app main.py
+++ app main.py
@@ -6,6 +6,7 @@ import numpy as np
 with open("tfidf_vectorizer.pkl", "rb") as f:
     vectorizer = pickle.load(f)
 
+
 # Load trained model
 with open("logistic_sentiment_model.pkl", "rb") as f:
     model = pickle.load(f)
